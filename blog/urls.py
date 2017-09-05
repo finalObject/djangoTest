e7f9094e5be9f78d24689ea40137eebfd5ce3d29
X. HU(Mac) <i@finalobject.cn>
2017-09-05 21:09:33 +0800

    base

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from . import views
 urlpatterns = [
-	url(r'',views.hello),
+	url(r'^$',views.hello),
 ]
+ 
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from . import models
 # Create your views here.
 
 def hello(request):
-	return HttpResponse('<html>helloWorld</html>')
+	article = models.Article.objects.get(pk=1)
+	return render(request,'blog/index.html',{'article':article})
+	#return HttpResponse('<html>helloWorld</html>') 
+ 
