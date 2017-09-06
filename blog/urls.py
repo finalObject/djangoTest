9bb0477497bae1e9ba2a7820493aeb17b26a1301
X. HU(Mac) <i@finalobject.cn>
2017-09-06 17:11:21 +0800

    good morning

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url,include
 from django.contrib import admin
 from . import views
+
+
 urlpatterns = [
-	url(r'^$',views.hello),
+	url(r'^$',views.index),
+	url(r'^(?P<id>[0-9]+)$',views.article_page,name='article_page'),
+	url(r'^edit$',views.edit_page,name='edit_page')
 ] 
  
--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,12 @@ from django.http import HttpResponse
 from . import models
 # Create your views here.
 
-def hello(request):
-	article = models.Article.objects.get(pk=1)
-	return render(request,'blog/index.html',{'article':article})
+def index(request):
+	articles = models.Article.objects.all()
+	return render(request,'blog/index.html',{'articles':articles})
 	#return HttpResponse('<html>helloWorld</html>') 
- 
+def article_page(request,id):
+ 	article = models.Article.objects.get(pk=id)
+ 	return render(request,'blog/article_page.html',{'article':article})
+def edit_page(request):
+ 	return render(request,'blog/edit_page.html')
