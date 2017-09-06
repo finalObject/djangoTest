25b6a4a87f70ae30f0633821112b074525e9c721
X. HU(Mac) <i@finalobject.cn>
2017-09-06 17:38:01 +0800

    best wishes

--- blog/views.py
+++ blog/views.py
@@ -16,3 +16,9 @@ def article_page(request,id):
  	return render(request,'blog/article_page.html',{'article':article})
 def edit_page(request):
  	return render(request,'blog/edit_page.html')
+def edit_action(request):
+	title = request.POST.get('title','TITLE')
+	content = request.POST.get('content','CONTENT')
+	models.Article.objects.create(title=title,content=content)
+	articles = models.Article.objects.all()
+	return render(request,'blog/index.html',{'articles':articles}) 
