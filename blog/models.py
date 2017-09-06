b8e933d96dcd75e4b1653761cd639517933768d0
X. HU(Mac) <i@finalobject.cn>
2017-09-06 09:49:59 +0800

    good morning

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,4 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
 	title =  models.CharField(max_length=32,default='Title')
-	content = models.TextField(null=True)
+	content = models.TextField(null=True) 
